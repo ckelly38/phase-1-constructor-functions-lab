9cb6e6fcbf842ad60798400f217a3e00ba758cb1
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-07-12 21:46:02 -0600

    basic constructor example

---
+++ index.js
@@ -0,0 +1,25 @@
+class Scooter{
+    constructor(yr, clrstr, mdlstr)
+    {
+        this.year = yr;
+        this.color = clrstr;
+        this.model = mdlstr;
+    }
+}
+
+class Driver{
+    constructor(nm, ag, exprc)
+    {
+        this.name = nm;
+        this.age = ag;
+        this.experience = exprc;
+    }
+}
+
+class PickupLocation{
+    constructor(addr, cty)
+    {
+        this.address = addr;
+        this.city = cty;
+    }
+}
